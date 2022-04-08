75f64b84a4564746f06046935d379f272b55adb8
srendonsoto2123 <srendonsoto@gmail.com>
2022-04-08 10:30:17 -0500

    Modificando la configuracion del treesitter

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -1,4 +1,6 @@
 require("nvim-treesitter.configs").setup {
+  ensure_installed = { "rust", "go", "lua", "c_sharp", "dockerfile", "python", "solidity", "yaml" },
+
   sync_install = false,
 
   highlight = {
