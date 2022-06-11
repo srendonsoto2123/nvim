a092d3f04e7909298026c4444fdbbfd60ae41fb4
srendonsoto2123 <srendonsoto@gmail.com>
2022-06-11 10:41:43 -0500

    Activando nvim-ts-autotag y añadiendo notificacion de fallo de carga para nvim-ts-autotag

--- lua/settings/plugins/closetag.lua
+++ lua/settings/plugins/closetag.lua
@@ -1,9 +1,10 @@
-local status_closetag, closetag = pcall( require, 'nvim-ts-closetag' )
+local status_closetag, closetag = pcall( require, 'nvim-ts-autotag' )
 
 if not status_closetag then
+   vim.notify("Ha ocurrido un error de carga\nEl plugin no está instalado", "error", {
+      title = "nvim-ts-autotag",
+   })
    return
 end
 
-closetag.setup({
-   filetypes = { "html", "xml", "tsx", "jsx" }
-})
+closetag.setup()
