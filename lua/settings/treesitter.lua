8483238a39c16e9aaed9d0f34a12755925ac7065
srendonsoto2123 <srendonsoto@gmail.com>
2022-10-13 19:17:50 -0500

    Corrigiendo treesitters

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -1,28 +1,29 @@
-local status_treesitter, treesitter_configs = pcall(require, "nvim-tresitter.configs") 
+local status_treesitter, treesitter_configs = pcall(require, "nvim-treesitter.configs")
 
 if not status_treesitter then
-   return
+  vim.notify(treesitter_configs)
+  return
 end
 
 treesitter_configs.setup {
   ensure_installed = {
-     "rust",
-     "go",
-     "lua",
-     "c_sharp",
-     "dockerfile",
-     "python",
-     "solidity",
-     "yaml",
-     "css",
-     "html",
-     "javascript",
-     "json",
-     "scss",
-     "toml",
-     "tsx",
-     "typescript",
-     "bash",
+    "rust",
+    "go",
+    "lua",
+    "c_sharp",
+    "dockerfile",
+    "python",
+    "solidity",
+    "yaml",
+    "css",
+    "html",
+    "javascript",
+    "json",
+    "scss",
+    "toml",
+    "tsx",
+    "typescript",
+    "bash",
   },
 
   sync_install = true,
@@ -32,6 +33,6 @@ treesitter_configs.setup {
     additional_vim_regex_highlighting = false,
   },
   indent = {
-     enable = false,
+    enable = false,
   },
 }
