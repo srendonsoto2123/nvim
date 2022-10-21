159cc5a96df356d4462f1ad4b00a47f687708ea5
srendonsoto2123 <srendonsoto@gmail.com>
2022-10-20 23:04:41 -0500

    Configuración básica para Mason

--- lua/core/lsp/init.lua
+++ lua/core/lsp/init.lua
@@ -9,7 +9,6 @@
 --
 --require("mapping")("lsp")
 --require("core.lsp.config")
---require("core.lsp.handlers").setup()
 --require("core.lsp.null_ls")
 
 local mason_status, mason = pcall(require, "mason")
@@ -20,35 +19,44 @@ if not mason_status then
   vim.notify(mason, "error", {
     title = "Mason.nvim"
   })
+  return
 end
 
 if not mason_lspconfig_status then
   vim.notify(mason_lspconfig, "error", {
     title = "Mason-lspconfig"
   })
+  return
 end
 
 if not lspconfig_status then
   vim.notify(lspconfig, "error", {
     title = "Lspconfig"
   })
+  return
 end
 
-mason.setup()
-mason_lspconfig.setup({
-     ensure_installed = { "rust_analyzer", "sumneko_lua", "dockerls", "elixirls" },
+mason.setup({
      ui = {
         icons = {
-           server_installed = "✓",
-           server_pending = "➜",
-           server_unistalled = "✗",
-      }
+           package_installed = "✓",
+           package_pending = "➜",
+           package_uninstalled = "✗",
+      },
+      border = "rounded"
     }
+})
+
+mason_lspconfig.setup({
+     ensure_installed = { "rust_analyzer", "sumneko_lua", "dockerls", "elixirls",
+                          "eslint", "yamlls", "bashls", "jsonls", "pyright"},
   }
 )
 
 mason_lspconfig.setup_handlers({
   function(server_name)
     lspconfig[server_name].setup({})
+    require("core.lsp.handlers").setup()
+    require("mapping")("lsp")
   end,
 })
